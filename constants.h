b8753d935737fda33601a0a9bacf6c8ba13e359a
Abhay D <a.deshpande012@gmail.com>
2021-06-04 23:17:38 -0700

    added landmark fov, decreased odom noise

--- constants.h
+++ constants.h
@@ -18,6 +18,7 @@ const double OBSTACLE_MAX_SIZE = ROBOT_LENGTH * 15; // m. used for efficient cal
 // I define a lot of these constants in terms of ROBOT_LENGTH to make it easy to change the scale
 
 const double LANDMARK_MAX_RANGE = ROBOT_LENGTH * 15; // m
+const double LANDMARK_HALF_FOV = 60 * M_PI / 180.0; // radians, half of the landmark fov
 const double LIDAR_MAX_RANGE = ROBOT_LENGTH * 5.6; // m
 const double LIDAR_MIN_RANGE = ROBOT_LENGTH; // m
 const int LIDAR_RESOLUTION = 100; // number of scans in the lidar fov
@@ -28,7 +29,7 @@ const double CORRUPTION_STD = 0.04; // std in meters at 1 meter distance
 const double DATA_LOSS_THRESHOLD = -2.0; // if stdn() is less than this, erase data
 const double GPS_POS_STD = 2.0; // m
 const double GPS_THETA_STD = M_PI/24; // rad (yes, I know this uses a magnetometer, not a GPS)
-const double WHEEL_STD = 0.04; // std in meters for 1 meter distance traveled
+const double WHEEL_STD = 0.005; // std in meters for 1 meter distance traveled
                                // variance increases for longer distances
 
 /* Graphics */
